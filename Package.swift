0dc5c92acf23701337f325e7fdc8b924bc8ebfe4
Matthew Ramsden <matthew.ramsden@hcahealthcare.com>
2020-04-07 12:35:13 -0500

    spm: added tools

--- Package.swift
+++ Package.swift
@@ -22,6 +22,7 @@
 //  THE SOFTWARE.
 //
 
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(name: "CodableKeychain", dependencies: [], exclude: ["Tests", "MobileHost"])
