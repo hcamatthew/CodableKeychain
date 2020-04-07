a301ed99335b89097e53436d0796a850fb493e31
Matthew Ramsden <matthew.ramsden@hcahealthcare.com>
2020-04-07 12:54:55 -0500

    spm: package update

--- Package.swift
+++ Package.swift
@@ -1,8 +1,19 @@
 // swift-tools-version:5.2
+
 import PackageDescription
 
 let package = Package(
     name: "CodableKeychain",
-    dependencies: [],
-    exclude: ["Tests", "MobileHost"]
+    platforms: [
+        .ios(.v13.0),
+    ],
+    products: [
+        .library(name: "CodableKeychain", targets: ["CodableKeychain"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(name: "CodableKeychain", dependencies: ["Utility"]),
+        .testTarget(name: "CodableKeychainTests", dependencies: ["CodableKeychain"]),
+    ]
 )
